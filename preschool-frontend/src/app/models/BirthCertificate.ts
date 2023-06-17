5693e3f2a8c0597ce44e93ba0e86460c99099de0
nananeko1305 <nananeko1305@hotmail.com>
2023-06-18 01:19:28 +0200

    PDF and view of registry

---
+++ preschool-frontend/src/app/models/BirthCertificate.ts
@@ -0,0 +1,12 @@
+export class BirthCertificate {
+  ime = ""
+  prezime = ""
+  ime_oca = ""
+  jmbg_oca = ""
+  ime_majke = ""
+  jmbg_majke = ""
+  datum_rodjenja = 0
+  mesto_rodjenja = ""
+  jmbg = ""
+  pol = ""
+}
---
+++ preschool-frontend/src/app/models/ExtractFromTheDeathRegister.ts
@@ -0,0 +1,14 @@
+export class ExtractFromTheDeathRegister {
+  ime = ""
+  prezime = ""
+  ime_oca = ""
+  jmbg_oca = ""
+  ime_majke = ""
+  jmbg_majke = ""
+  datum_rodjenja = 0
+  mesto_rodjenja = ""
+  jmbg = ""
+  pol = ""
+  datim_smrti = 0
+  mesto_smrti = ""
+}
